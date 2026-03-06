6eaf0d5640bcc94482ee153309a5dd1903eecebd
mateus <matturushihara@gmail.com>
2026-03-06 17:32:11 -0300

    feat: trocando o banco de dados sql pelo banco do postgresql e adicionando as variaveis do ambiente

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+from dotenv import load_dotenv
+import os
 
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4%yyx&om!low@zn1(0b9jqd(vd!f9gr0l6y09z7g2bm7k+_4r!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'FALSE') == 'TRUE'
 
 ALLOWED_HOSTS = []
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #adiconar os meus apps
+    'rest_Framework',
+    'chat',
+    'data',
+    
 ]
 
 MIDDLEWARE = [
@@ -71,11 +81,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+#trocar a base de dados
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST' : os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
--- backend/docu/Postgresql.md
+++ backend/docu/Postgresql.md
@@ -12,3 +12,8 @@ pg admin e criar a base
 # Abra o pgAdmin (foi instalado junto com o PostgreSQL), conecte com sua senha e rode essa query:
 sqlCREATE DATABASE lifenavigator;
 # Ou se preferir pelo terminal, abra o SQL Shell (psql) que veio na instalação e rode o mesmo comando.
+
+agora criar o .env.
+
+# Passo 3 — Atualizar o settings.py
+# Abra config/settings.py e substitua o bloco DATABASES e adicione o dotenv:
